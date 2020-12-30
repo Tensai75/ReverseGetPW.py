27a5865c016d5a0b2465df5a453a24d21e4afd6a
Tensai <tensai@gmx.net>
2020-12-30 12:08:12 +0100

    Update ReverseGetPW.py

--- ReverseGetPW.py
+++ ReverseGetPW.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python2.7
 ##############################################################################
 ### NZBGET POST-PROCESSING SCRIPT                                          ###
+
+# Reverse Get Password
 #
-# Renames the final destination directory to "title{{password}}" if a password was provided
 #
+# Renames the final destination directory to "title{{password}}" if a password was provided.
+#
+# This might be usefull in some special usecases where having the password in the name of the destination folder might be desired.
+#
+# NOTE: If you have other post-processing scripts running, this extension script should probably be the last post-processing script to run
+#
+#
+# NOTE: This script requires Python to be installed on the system running NZBGet.
+
 ### NZBGET QUEUE SCRIPT                                                    ###
 ##############################################################################
 
