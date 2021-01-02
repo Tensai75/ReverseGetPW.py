e1a361d981ce7e8588c8c2ffe085d429fdf0250e
Tensai <tensai@gmx.net>
2021-01-02 14:16:09 +0100

    Rewritten for python 3.x
    Completely rewritten for python 3.x with much more verbose log output

--- ReverseGetPW.py
+++ ReverseGetPW.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2.7
+#!/usr/bin/python3
 ##############################################################################
 ### NZBGET POST-PROCESSING SCRIPT                                          ###
 
@@ -12,7 +12,7 @@
 # NOTE: If you have other post-processing scripts running, this extension script should probably be the last post-processing script to run
 #
 #
-# NOTE: This script requires Python to be installed on the system running NZBGet.
+# NOTE: This script requires Python 3.x to be installed on the system running NZBGet.
 
 ### NZBGET QUEUE SCRIPT                                                    ###
 ##############################################################################
@@ -21,26 +21,36 @@ import re
 import sys
 import os
 
-# errorHandler function
-def errorHandler(m, e):
-    print '[ERROR] %s. Error: %s'%(m, e)
-    sys.exit(94)
-
-
-if os.environ.get('NZBPP_FINALDIR'):
-    directory = os.environ.get('NZBPP_FINALDIR')
-else:
-    directory = os.environ.get('NZBPP_DIRECTORY')
 password = os.environ.get('NZBPR_*Unpack:Password')
-if (directory and password):
+if (password):
+    print('[INFO] Unpack password is set to: %s' %(password))
+    if os.environ.get('NZBPP_FINALDIR'):
+        directory = os.environ.get('NZBPP_FINALDIR')
+    else:
+        directory = os.environ.get('NZBPP_DIRECTORY')
+    print('[INFO] Current destination directory is set to: %s' %(directory))
     pattern = re.compile("^.*\{\{" + re.escape(password) + "\}\}$")
     match = pattern.search(directory)
     if (not match):
-        newdirectory = directory + "{{" + password + "}}"
-        try:
-            os.rename(directory, newdirectory)
-        except Exception, e:
-            errorHandler('Cannot rename directory ' + directory, str(e))
-        print '[NZB] DIRECTORY=' + newdirectory
-        print '[NZB] FINALDIR=' + newdirectory
-sys.exit(93)
+        pattern = re.compile("[" + re.escape(':*/"?>|<') + "]")
+        match = pattern.search(password)
+        if (match):
+            print('[ERROR] The password contains invalid characters! Cannot rename the destination directory.')
+            sys.exit(94)
+        else:
+            newdirectory = directory + "{{" + password + "}}"
+            try:
+                print('[INFO] Renaming the destination directory to: %s' %(newdirectory))
+                os.rename(directory, newdirectory)
+                print('[NZB] DIRECTORY=%s' %(newdirectory))
+                print('[NZB] FINALDIR=%s' %(newdirectory))
+                print('[INFO] Destination directory sucessfully renamed to: %s' %(newdirectory))
+                sys.exit(93)
+            except Exception as e:
+                print('[ERROR] Cannot rename directory "%s". Error: %s' %(directory, str(e)))
+                sys.exit(94)
+    else:
+        print('[INFO] The destination directory already contains the password in currly braces. No rename required.')
+else:
+    print('[INFO] No unpack password was set. No rename required.')
+sys.exit(95)
